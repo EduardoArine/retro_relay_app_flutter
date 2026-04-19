6c4e52b7901d87f6a9e7916271b1e4c1aec4f704
EduardoArine <eduardoarine@yahoo.com.br>
2026-04-19 13:07:03 -0300

    Refactor code structure for improved readability and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -6,7 +7,9 @@ import 'app/app.dart';
 import 'features/config/data/prefs_service.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
+
   final prefs = await SharedPreferences.getInstance();
 
   runApp(
@@ -15,4 +18,6 @@ void main() async {
       child: const RetroRelayApp(),
     ),
   );
+
+  FlutterNativeSplash.remove();
 }
